f1acd158cb261a5f9ef02fbe5565ed3891ad1059
hhhan-jh <jeo4924@gmail.com>
2025-05-20 20:10:41 +0900

    Feat: 유저 프로필 업데이트 API 추가
    프로필 사진 정보와 유저 기본정보(닉네임, 포지션, 대학)을 서비스 분리하여 작업

--- src/main/java/com/jandi/band_backend/user/controller/UserController.java
+++ src/main/java/com/jandi/band_backend/user/controller/UserController.java
@@ -2,19 +2,20 @@ package com.jandi.band_backend.user.controller;
 
 import com.jandi.band_backend.global.ApiResponse;
 import com.jandi.band_backend.security.jwt.JwtTokenProvider;
+import com.jandi.band_backend.user.dto.UpdateUserInfoReqDTO;
 import com.jandi.band_backend.user.dto.UserInfoDTO;
+import com.jandi.band_backend.user.service.UserPhotoService;
 import com.jandi.band_backend.user.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
+    private final UserPhotoService userPhotoService;
     private final JwtTokenProvider jwtTokenProvider;
 
     @GetMapping("/me/info")
@@ -23,7 +24,28 @@ public class UserController {
     ) {
         String accessToken = token.replace("Bearer ", "");
         String kakaoOauthId = jwtTokenProvider.getKakaoOauthId(accessToken);
-        UserInfoDTO userInfo = userService.getMyInfo(kakaoOauthId);
+
+        // 유저 기본 정보 및 프로필 정보 조회
+        UserInfoDTO userInfo = new UserInfoDTO(
+                userService.getMyInfo(kakaoOauthId),
+                userPhotoService.getMyPhoto(kakaoOauthId)
+        );
         return ApiResponse.success("내 정보 조회 성공", userInfo);
+    }
+
+    @PatchMapping("/me/info")
+    public ApiResponse<UserInfoDTO> updateMyInfo(
+            @RequestHeader("Authorization") String token,
+            @ModelAttribute UpdateUserInfoReqDTO updateDTO,
+            @RequestPart(value = "profilePhoto", required = false) MultipartFile profilePhoto
+
+    ) {
+        String accessToken = token.replace("Bearer ", "");
+        String kakaoOauthId = jwtTokenProvider.getKakaoOauthId(accessToken);
+
+        // 유저 기본 정보 및 프로필 사진 수정
+        userService.updateMyInfo(kakaoOauthId, updateDTO);
+        userPhotoService.updateMyPhoto(kakaoOauthId, profilePhoto);
+        return ApiResponse.success("내 정보 수정 성공");
     }
 }
---
+++ src/main/java/com/jandi/band_backend/user/service/UserPhotoService.java
@@ -0,0 +1,70 @@
+package com.jandi.band_backend.user.service;
+
+import com.jandi.band_backend.global.exception.UserNotFoundException;
+import com.jandi.band_backend.image.service.S3Service;
+import com.jandi.band_backend.user.entity.UserPhoto;
+import com.jandi.band_backend.user.entity.Users;
+import com.jandi.band_backend.user.repository.UserPhotoRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class UserPhotoService {
+    private final S3Service s3Service;
+    private final UserService userService;
+    private final UserPhotoRepository userPhotoRepository;
+
+    private final String kakaoPrefix = "k.kakaocdn.net";
+    private final String s3DirName = "user-photo";
+
+    /// 유저 프로필 사진 조회
+    public UserPhoto getMyPhoto(String kakaoOauthId) {
+        Users user = userService.getMyInfo(kakaoOauthId);
+        UserPhoto userProfile = userPhotoRepository.findByUser(user);
+
+        if (userProfile == null) throw new UserNotFoundException();
+        return userProfile;
+    }
+
+    /// 유저 프로필 사진 수정
+    public void updateMyPhoto(String kakaoOauthId, MultipartFile newProfileFile) {
+        // 프로필 사진이 없을 경우 수정하지 않음
+        if (newProfileFile == null || newProfileFile.isEmpty()) return;
+
+        // 프로필 조회
+        UserPhoto profile = getMyPhoto(kakaoOauthId);
+        String originalUrl = profile.getImageUrl();
+
+        // 새 이미지 업로드 및 이전 이미지 삭제
+        String newUrl = uploadNewProfileFile(newProfileFile);
+        deleteOriginalProfileFile(originalUrl);
+        profile.setImageUrl(newUrl);
+        userPhotoRepository.save(profile);
+    }
+
+    /// 내부 메서드
+    // S3에 새 프로필 이미지 업로드
+    private String uploadNewProfileFile(MultipartFile newProfileFile) {
+        try{
+            return s3Service.uploadImage(newProfileFile, s3DirName);
+        }catch (Exception e) {
+            throw new RuntimeException("프로필 사진 업로드 실패: " + e.getMessage());
+        }
+    }
+
+    // S3에서 이전 프로필 이미지 삭제
+    private void deleteOriginalProfileFile(String originalProfileUrl) {
+        // 카카오 기본 프로필인 경우 삭제 작업을 잔행하지 않음
+        if(originalProfileUrl.contains(kakaoPrefix)) return;
+
+        try{
+            s3Service.deleteImage(originalProfileUrl);
+        }catch (Exception e) {
+            log.error("프로필 사진 삭제 실패: url={}", originalProfileUrl);
+        }
+    }
+}

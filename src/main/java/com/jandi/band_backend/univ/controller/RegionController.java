6dd38fe7d1f27f18499123dc784e2b6a749c895a
hhhan-jh <jeo4924@gmail.com>
2025-05-22 22:51:08 +0900

    Feat: 지역 조회 API 추가

---
+++ src/main/java/com/jandi/band_backend/univ/controller/RegionController.java
@@ -0,0 +1,25 @@
+package com.jandi.band_backend.univ.controller;
+
+import com.jandi.band_backend.global.ApiResponse;
+import com.jandi.band_backend.univ.dto.RegionRespDTO;
+import com.jandi.band_backend.univ.entity.Region;
+import com.jandi.band_backend.univ.service.RegionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/region")
+@RequiredArgsConstructor
+public class RegionController {
+    private final RegionService regionService;
+
+    @GetMapping("/all")
+    public ApiResponse<List<RegionRespDTO>> getAllRegion() {
+        List<RegionRespDTO> regionList = regionService.getAllRegions();
+        return ApiResponse.success("지역 리스트 조회 성공", regionList);
+    }
+}
---
+++ src/main/java/com/jandi/band_backend/univ/repository/RegionRepository.java
@@ -0,0 +1,8 @@
+package com.jandi.band_backend.univ.repository;
+
+import com.jandi.band_backend.univ.entity.Region;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RegionRepository extends JpaRepository<Region, Integer> {
+
+}

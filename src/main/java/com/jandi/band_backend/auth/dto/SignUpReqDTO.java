7d8f8dc1b26c08218744773a76176d49bf12ba4f
kyj0503 <heroria0503@gmail.com>
2025-05-24 00:42:31 +0900

    docs: Auth 패키지 Swagger 주석 추가

--- src/main/java/com/jandi/band_backend/auth/dto/SignUpReqDTO.java
+++ src/main/java/com/jandi/band_backend/auth/dto/SignUpReqDTO.java
@@ -1,12 +1,18 @@
 package com.jandi.band_backend.auth.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Schema(description = "회원가입 요청 DTO")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class SignUpReqDTO {
+    @Schema(description = "사용자 포지션 (악기 또는 역할)", example = "기타", required = true)
     private String position;
+    
+    @Schema(description = "대학교명", example = "서울대학교", required = true)
     private String university;
 }
--- src/main/java/com/jandi/band_backend/auth/dto/kakao/KakaoUserInfoDTO.java
+++ src/main/java/com/jandi/band_backend/auth/dto/kakao/KakaoUserInfoDTO.java
@@ -1,12 +1,19 @@
 package com.jandi.band_backend.auth.dto.kakao;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+@Schema(description = "카카오 사용자 정보 DTO")
 @Data
 @AllArgsConstructor
 public class KakaoUserInfoDTO {
+    @Schema(description = "카카오 OAuth ID", example = "123456789")
     private String kakaoOauthId;
+    
+    @Schema(description = "카카오 닉네임", example = "홍길동")
     private String nickname;
+    
+    @Schema(description = "카카오 프로필 사진 URL", example = "https://k.kakaocdn.net/dn/...")
     private String profilePhoto;
 }

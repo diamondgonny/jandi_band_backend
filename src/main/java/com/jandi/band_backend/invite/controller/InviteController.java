b6ff3fa680487072e36746735224390d6f189654
hhhan-jh <jeo4924@gmail.com>
2025-05-27 07:52:24 +0900

    docs: 스웨거 적용

--- src/main/java/com/jandi/band_backend/invite/controller/InviteController.java
+++ src/main/java/com/jandi/band_backend/invite/controller/InviteController.java
@@ -4,6 +4,8 @@ import com.jandi.band_backend.global.CommonResponse;
 import com.jandi.band_backend.invite.dto.InviteLinkRespDTO;
 import com.jandi.band_backend.invite.service.InviteService;
 import com.jandi.band_backend.security.CustomUserDetails;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,12 +13,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Invite API")
 @RestController
 @RequestMapping("api/invite")
 @RequiredArgsConstructor
 public class InviteController {
     private final InviteService inviteService;
 
+    @Operation(summary = "동아리 초대 링크 생성")
     @PostMapping("/clubs/{clubId}")
     public CommonResponse<InviteLinkRespDTO> inviteClub(
             @AuthenticationPrincipal CustomUserDetails userDetails,
@@ -27,6 +31,7 @@ public class InviteController {
         return CommonResponse.success("동아리 초대 링크 생성 성공", inviteLinkRespDTO);
     }
 
+    @Operation(summary = "팀 초대 링크 생성")
     @PostMapping("/teams/{teamId}")
     public CommonResponse<InviteLinkRespDTO> inviteTeam(
             @AuthenticationPrincipal CustomUserDetails userDetails,

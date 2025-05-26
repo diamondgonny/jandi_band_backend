dcbd0a331ce0353d7cccab471036dac73b50027b
hhhan-jh <jeo4924@gmail.com>
2025-05-27 06:23:38 +0900

    refactor: 사용하지 않는 임포트문 제거

--- src/main/java/com/jandi/band_backend/invite/controller/JoinController.java
+++ src/main/java/com/jandi/band_backend/invite/controller/JoinController.java
@@ -1,17 +1,9 @@
 package com.jandi.band_backend.invite.controller;
 
-import com.jandi.band_backend.club.dto.ClubDetailRespDTO;
-import com.jandi.band_backend.club.dto.ClubReqDTO;
 import com.jandi.band_backend.global.CommonResponse;
-import com.jandi.band_backend.invite.dto.InviteLinkRespDTO;
 import com.jandi.band_backend.invite.service.JoinService;
 import com.jandi.band_backend.security.CustomUserDetails;
-import io.swagger.v3.oas.annotations.Operation;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.hibernate.annotations.Parameter;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
